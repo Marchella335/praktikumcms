a324350bf9a0422172fca4d20869b2a63d20a7f3
Marchella335 <231210059@student.mercubuana-yogya.ac.id>
2025-06-06 16:22:21 +0700

    Initial Commit

---
+++ app/Http/Controllers/JANJI_TEMUController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class JANJI_TEMUController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/REKAM_MEDISController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class REKAM_MEDISController extends Controller
+{
+    //
+}
